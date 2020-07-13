c7cf4765104a1e501e53cce8a976bb878fa80015
Jody Hunt <jodyhunt@hotmail.com>
2020-07-13 07:32:21 -0500

    Create terr-demo-api-key.tf

---
+++ terr-demo-api-key.tf
@@ -0,0 +1,18 @@
+variable "GITBOT_UNAME" {
+  description = "GitBot user name"
+  default = "default"
+}
+
+variable "GITBOT_TOKEN" {
+  description = "Gitbot token"
+  default = "default"
+}
+
+output "Gitbot token" {
+  sensitive = true
+  value = "${var.GITBOT_TOKEN}"
+}
+
+output "Gitbot Uname" {
+  value = "${var.GITBOT_UNAME}"
+}
